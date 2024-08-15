9220e95162c5d8685d62110f595ad1dc396a4fa9
nomankarim8 <karimnoman97@gmail.com>
2024-08-15 09:15:12 -0700

    update Installer.py

--- Installer.py
+++ Installer.py
@@ -8,6 +8,6 @@ os.system("pkg install -y root-repo")
 os.system("pkg install -y git tsu python wpa-supplicant pixiewps iw")
 os.system("cd ..;git clone https://github.com/nomankarim8/Wifi_Hack.git")
 
-os.system("cd ..;chmod +x Wifi_Hack/nomankarim.py")
+os.system("cd ..;chmod +x Wifi_Hack/nomanhack.py")
 
 print("\033[1;34;40mThanks.\nInstallation Done.\nNow Go To Home Directory[~] And Enter This Command :\nsudo python Wifi_Hack/birihack.py -i wlan0 -K")

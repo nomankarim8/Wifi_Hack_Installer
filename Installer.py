b2f1b3dfb14d4d8f4bf94dc3c9bafecfcb4017ce
nomankarim8 <karimnoman97@gmail.com>
2024-08-15 09:07:17 -0700

    update Installer.py

--- Installer.py
+++ Installer.py
@@ -1,9 +1,9 @@
 import os
-print('''\033[1;36;40mWifi_Hack Installer By THBD
+print('''\033[1;36;40mWifi_Hack Installer By NOMAN
 Your Device Must Be Rooted
 If Any Question,
-Contact Me On Telegram
-Tg_User:@biri_baba \n''')
+Contact Me On Faebook
+FB_User:@noman.karim.8 \n''')
 os.system("pkg install -y root-repo")
 os.system("pkg install -y git tsu python wpa-supplicant pixiewps iw")
 os.system("cd ..;git clone https://github.com/Mahfuz-THBD/Wifi_Hack")
